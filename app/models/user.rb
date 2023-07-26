db66cb617ac89d5711cd677e417f9a054d3dad56
Ibnballo1 <belloabdullateef035@gmail.com>
2023-07-26 17:54:17 +0100

    Add models for user and debt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :debts
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 50 }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 6 }
 end
