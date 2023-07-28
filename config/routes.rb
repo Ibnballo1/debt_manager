c0b42b09a5d9a24b172096cdb16045fad1502295
Ibnballo1 <belloabdullateef035@gmail.com>
2023-07-28 09:36:32 +0100

    Add routes for users and debts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'debts/index'
+  # get 'debts/index'
+  resources :users do
+    resources :debts
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
