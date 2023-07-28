c3a3b5d4a7aab8ecfdac49d3fa434d99248a89e3
Ibnballo1 <belloabdullateef035@gmail.com>
2023-07-28 18:36:37 +0100

    Update debit controller

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -1,4 +1,37 @@
 class DebtsController < ApplicationController
+  before_action :set_user, only: [:index, :show]
+  before_action :set_debtor, only: [:show]
   def index
+    @debts = @user.debts
+  end
+
+  # method to create new user
+  def new
+  end
+
+  # method to show new user
+  def show
+    @debt
+  end
+
+  # edit user
+  def edit
+  end
+
+  # Update user
+  def update
+  end
+
+  # Destroy user
+  def delete
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_debtor
+    @debt = Debt.find(params[:id])
   end
 end
