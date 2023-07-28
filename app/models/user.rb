5bdb27a210ede70e904f1a593ef8cbb7eafeb721
Ibnballo1 <belloabdullateef035@gmail.com>
2023-07-28 11:46:11 +0100

    Add method for all the debts for a particular user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 50 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 6 }
+
+  # method to display all debts for a user
+  def debtors
+    @debtors = Debt.all
+  end
 end
