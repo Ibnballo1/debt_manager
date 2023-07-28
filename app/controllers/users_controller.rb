86da42fbdc73f85c8e4890d1a09f65570e46b8bf
Ibnballo1 <belloabdullateef035@gmail.com>
2023-07-28 12:16:21 +0100

    Update action index in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  def index
+    @users = User.all
+  end
+
+  # method to create new user
+  def new
+  end
+
+  # method to show new user
+  def show
+    @user
+  end
+
+  # edit user
+  def edit
+  end
+
+  # Update user
+  def update
+  end
+
+  # Destroy user
+  def delete
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
