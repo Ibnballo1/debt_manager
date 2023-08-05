3893e26f4aa011c74a9819ca1c4ecc8d3ab94955
Ibnballo1 <belloabdullateef035@gmail.com>
2023-08-05 15:00:17 +0100

    Add reason column to debts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_05_135440) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_05_135739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_05_135440) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "debtor_id", null: false
+    t.string "reason"
     t.index ["debtor_id"], name: "index_debts_on_debtor_id"
     t.index ["user_id"], name: "index_debts_on_user_id"
   end
