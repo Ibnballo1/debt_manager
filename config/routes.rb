9208406dbfc572e3e35551bcee093fc1b7ae5745
Ibnballo1 <belloabdullateef035@gmail.com>
2023-08-05 15:18:12 +0100

    Update router

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users do
+  resources :users
+  resources :debtors do
     resources :debts
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
