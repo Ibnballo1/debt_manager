d5e1332c2e26b5e31760f1f73aa35ab66ba27bd4
Ibnballo1 <belloabdullateef035@gmail.com>
2023-08-11 14:58:05 +0100

    Seed databse

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# users = User.create(username: 'Ibnballo', email: 'testing@gmail.com', password: '123456')
+users = User.create(username: 'Ibnballo', email: 'testing@gmail.com', password: '123456')
