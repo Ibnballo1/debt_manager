f017535fbe6c6c769c78e1741c308df20761c90e
Ibnballo1 <belloabdullateef035@gmail.com>
2023-08-14 16:29:58 +0100

    Change username to name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_201935) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_14_145926) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_201935) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "name"
     t.string "email"
     t.string "password"
     t.datetime "created_at", null: false
