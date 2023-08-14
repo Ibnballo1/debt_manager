0ad98714d755113839a6b3d3ce256bd5f1a52338
Ibnballo1 <belloabdullateef035@gmail.com>
2023-08-14 17:14:33 +0100

    Remove password from user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_14_145926) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_14_153717) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_14_145926) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
