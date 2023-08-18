19bc2be4d84f5547bae332841ef018d1f6c9c781
Ibnballo1 <belloabdullateef035@gmail.com>
2023-08-18 15:53:52 +0100

    Add parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   def after_sign_up_path_for(resource)
     debts_path(resource) # Redirect to the user's profile page after sign-up
