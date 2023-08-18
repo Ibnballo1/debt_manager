ca92f9cbba1a6cfba772ca807d6cc7b944de0b2f
Ibnballo1 <belloabdullateef035@gmail.com>
2023-08-18 16:26:33 +0100

    Add user_id reference to debts

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,4 +1,6 @@
 class Debt < ApplicationRecord
+  belongs_to :user
+
   validates :debtor, presence: true, length: { minimum: 3, maximum: 100 }
   validates :due_date, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_14_153717) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_18_150029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_14_153717) do
     t.boolean "is_paid", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_debts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_14_153717) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "debts", "users"
 end
