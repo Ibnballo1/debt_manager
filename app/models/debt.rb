4c2930dbd59d8cd57b6daa5aee4a2a1e3d98e277
Ibnballo1 <belloabdullateef035@gmail.com>
2023-08-21 14:57:06 +0100

    Add authentication

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -4,4 +4,14 @@ class Debt < ApplicationRecord
   validates :debtor, presence: true, length: { minimum: 3, maximum: 100 }
   validates :due_date, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
+
+  def due_day(debt_date, due_date)
+    date_of_debt = debt_date.to_date
+    exp_day = (due_date - date_of_debt).to_i
+    if exp_day < 1
+      return "Your Debt is due"
+    else
+      return "Due: in #{exp_day} Days"
+    end
+  end
 end
