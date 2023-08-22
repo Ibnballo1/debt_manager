2fdd7454a938491c624365b28c03d03d72a2fc38
Ibnballo1 <belloabdullateef035@gmail.com>
2023-08-22 20:59:29 +0100

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # Catch all CanCan errors and alert the user of the exception
+  rescue_from CanCan::AccessDenied do | exception |
+    redirect_to debts_url, alert: "Access denied! You are not authorized to perform this action."
+  end
+
   protected
   
   def configure_permitted_parameters
@@ -7,11 +13,11 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_up_path_for(resource)
-    debts_path(resource) # Redirect to the user's profile page after sign-up
+    debts_path(resource) # Redirect to the debts page after sign-up
   end
 
   def after_sign_in_path_for(resource)
-    debts_path(resource) # Redirect to the user's profile page after sign-up
+    debts_path(resource) # Redirect to the debts page after sign-in
   end
 
   def after_sign_out_path_for(resource)
