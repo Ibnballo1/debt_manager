7cbc13aa9a5d0d70f3c692876b471e43151f23ba
Ibnballo1 <belloabdullateef035@gmail.com>
2023-08-27 15:44:06 +0100

    Add test to model using shoulda

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-#   subject {User.new(name: 'Abdullateef', email: 'user1@gmail.com', password: '123456')}
+  subject {User.new(name: 'Abdullateef', email: 'user1@gmail.com', password: '123456')}
 
-#   before { subject.save }
+  before { subject.save }
 
+  it 'should not be valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+  it 'should not be valid without an email' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_least(3) }
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password) }
 end
