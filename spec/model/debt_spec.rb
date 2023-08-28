31c49776147b54f94881454a118fe2e90507b18b
Ibnballo1 <belloabdullateef035@gmail.com>
2023-08-28 21:36:33 +0100

    Add test to debt model

--- spec/model/debt_spec.rb
+++ spec/model/debt_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Debt, type: :model do
-  
+  let(:user) {User.new(name: 'Abdullateef', email: 'user1@gmail.com', password: '123456')}
+  let(:debt) {Debt.new}
+
+  it 'should not be valid without a debtor' do
+    debt.debtor = nil
+    expect(debt).to_not be_valid
+  end
+
+  it 'should not be valid with a short debtor' do
+    debt.debtor = 'az'
+    expect(debt).to_not be_valid
+  end
+
+  it 'should not be valid with a longer debtor' do
+    debt.debtor = 'az' * 101
+    expect(debt).to_not be_valid
+  end
+
+  it 'should not be valid without due_date' do
+    debt.due_date = nil
+    expect(debt).to_not be_valid
+  end
+
+  it 'should not be valid without an amount' do
+    debt.amount = nil
+    expect(debt).to_not be_valid
+  end
+
+  it 'should not be valid if amount <= 0' do
+    debt.amount = -1
+    expect(debt).to_not be_valid
+  end
 end
