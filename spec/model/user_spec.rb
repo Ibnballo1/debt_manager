31f56a61f53d2fcb11975a780651f16f2d210ec0
Ibnballo1 <belloabdullateef035@gmail.com>
2023-08-28 21:01:26 +0100

    Update user test

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,22 +1,5 @@
 require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   subject {User.new(name: 'Abdullateef', email: 'user1@gmail.com', password: '123456')}
-
-#   before { subject.save }
-
-#   it 'should not be valid without a name' do
-#     subject.name = nil
-#     expect(subject).to_not be_valid
-#   end
-#   it 'should not be valid without an email' do
-#     subject.email = nil
-#     expect(subject).to_not be_valid
-#   end
-#   it { should validate_presence_of(:name) }
-#   it { should validate_length_of(:name).is_at_least(3) }
-# end
-
 RSpec.describe User, type: :model do
   let(:user) { User.new }
 
