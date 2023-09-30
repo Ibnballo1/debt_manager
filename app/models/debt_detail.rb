2e1f8a220e97a71e4e0fa586ed6078ff5f7c585e
Ibnballo1 <belloabdullateef035@gmail.com>
2023-09-30 09:52:18 +0100

    Update

--- app/models/debt_detail.rb
+++ app/models/debt_detail.rb
@@ -1,3 +1,16 @@
 class DebtDetail < ApplicationRecord
   belongs_to :debt
+  accepts_nested_attributes_for :debt
+  # @@sum = 0
+
+  validates :due_date, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+
+  # def total_debt_amount(amt)
+  #   @@sum += amt
+  # end
+
+  def default_reason(reason)
+    (reason == nil) ? reason = 'Add a reason for your debt' : reason
+  end
 end
