d706b39f3f42f9fefe00d5aa597131afce8f3675
Ibnballo1 <belloabdullateef035@gmail.com>
2023-10-02 17:11:00 +0100

    Add sum of all amount for a specific creditor

--- app/models/debt_detail.rb
+++ app/models/debt_detail.rb
@@ -1,14 +1,12 @@
 class DebtDetail < ApplicationRecord
   belongs_to :debt
-  accepts_nested_attributes_for :debt
-  # sum = 0
 
   validates :due_date, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
 
-  # def total_debt_amount(amt)
-  #   sum += amt
-  # end
+  def self.total_amount(debt_id)
+    where(debt_id: debt_id).sum(:amount)
+  end
 
   def default_reason(reason)
     (reason == nil) ? reason = 'Add a reason for your debt' : reason
