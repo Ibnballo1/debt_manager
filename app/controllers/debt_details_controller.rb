83a63a4ba4668447c234b256440177c0cdb9698b
Ibnballo1 <belloabdullateef035@gmail.com>
2023-10-12 12:54:59 +0100

    Add details for a specific creditor

--- app/controllers/debt_details_controller.rb
+++ app/controllers/debt_details_controller.rb
@@ -1,6 +1,6 @@
 class DebtDetailsController < ApplicationController
   # load_and_authorize_resource
-  before_action :set_debt, only: [:show, :edit, :update, :destroy]
+  before_action :set_debt, only: [:show, :destroy]
   before_action :set_debt_detail, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -14,8 +14,20 @@ class DebtDetailsController < ApplicationController
 
   # method to create new user
   def new
-    @debt = Debt.find(params[:debt_id]) # You'll need to find the associated debt
-    @debt_detail = @debt.build_debt_detail # Build a new debt_detail associated with the debt
+    @debt_id = params[:id]
+
+    if @debt_id
+      @debt = Debt.find_by(id: @debt_id)
+      if @debt
+        @debt_detail = @debt.debt_details.build # Build a new debt_detail associated with the creditor
+      else
+        redirect_to debts_path, alert: "Creditor not found."
+      end
+    else
+      redirect_to debts_path, alert: "Missing creditor ID."
+      # @debt_detail = @debt.debt_details.build # Build a new debt_detail associated with the debt
+    end
+    # @debt = Debt.find(params[:debt_id]) # You'll need to find the associated debt
   end
 
   # method to show new user
@@ -28,7 +40,7 @@ class DebtDetailsController < ApplicationController
     @debt_detail = DebtDetail.new(debt_detail_params)
   
     if @debt_detail.save
-      redirect_to debts_path(), notice: "Debt was successfully created."
+      redirect_to debts_path, notice: "Debt was successfully created."
     else
       render :new
     end
@@ -42,7 +54,7 @@ class DebtDetailsController < ApplicationController
   # Update user
   def update
     if @debt_detail.update(debt_detail_params)
-      redirect_to debt_details_path(), notice: "Debt was successfully updated."
+      redirect_to debt_details_path(id: @debt_detail.debt_id), notice: "Debt was successfully updated."
     else
       render :edit
     end
@@ -51,7 +63,7 @@ class DebtDetailsController < ApplicationController
   # Destroy user
   def destroy
     @debt_detail.destroy
-    redirect_to debt_details_path
+    redirect_to debt_details_path(id: @debt_detail.debt_id)
   end
 
   private
@@ -65,6 +77,6 @@ class DebtDetailsController < ApplicationController
   end
 
   def debt_detail_params
-    params.require(:debt_detail).permit(:due_date, :amount, :is_paid, :reason, debt_attributes: [:creditor])
+    params.require(:debt_detail).permit(:due_date, :amount, :reason, :is_paid, :debt_id)
   end
 end
